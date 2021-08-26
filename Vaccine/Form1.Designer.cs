64a777576e28e58dd927bccfcce664e061985777
ResoluteMoney92 <shawndko2001@gmail.com>
2021-08-26 13:32:59 +0200

    Test

---
+++ Vaccine/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace Vaccine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbx1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbx2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(173, 76);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(138, 22);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(173, 124);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(138, 22);
+            this.txt2.TabIndex = 1;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(173, 165);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(138, 22);
+            this.txt3.TabIndex = 2;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(91, 298);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(220, 51);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "Register";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 259);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Province";
+            // 
+            // cbx1
+            // 
+            this.cbx1.FormattingEnabled = true;
+            this.cbx1.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cbx1.Location = new System.Drawing.Point(173, 211);
+            this.cbx1.Name = "cbx1";
+            this.cbx1.Size = new System.Drawing.Size(138, 24);
+            this.cbx1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(31, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(325, 40);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Vaccine Registration";
+            // 
+            // cbx2
+            // 
+            this.cbx2.FormattingEnabled = true;
+            this.cbx2.Items.AddRange(new object[] {
+            "Kwazulu Natal",
+            "Limpopo",
+            "Guateng",
+            "Wes Kaap",
+            "Noord Kaap ",
+            "Oos Kaap",
+            "Vrystaat",
+            "Noord Wes"});
+            this.cbx2.Location = new System.Drawing.Point(173, 252);
+            this.cbx2.Name = "cbx2";
+            this.cbx2.Size = new System.Drawing.Size(138, 24);
+            this.cbx2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 425);
+            this.Controls.Add(this.cbx2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbx1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Vaccine Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbx1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbx2;
+    }
+}
+
---
+++ Vaccine/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Vaccine
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-VDKG9I7U;Initial Catalog=Vaccine;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {   con.Open();
+            
+            SqlCommand cmd = new SqlCommand("insert into Register(Id, Surname, Name, Gender, Province)values (@Id, @Surname, @Name, @Gender, @Province)", con);
+            cmd.Parameters.AddWithValue("@Id", txt1.Text);
+            cmd.Parameters.AddWithValue("@Surname", txt2.Text);
+            cmd.Parameters.AddWithValue("@Name", txt3.Text);
+            cmd.Parameters.AddWithValue("@Gender", cbx1.Text);
+            cmd.Parameters.AddWithValue("@Province", cbx2.Text);
+            
+            
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Register Added");
+           
+        }
+        public static void main(string[] args)
+            {
+
+                Application.Run(new Form1());
+
+            }
+    }
+    }
+
+            
+        
